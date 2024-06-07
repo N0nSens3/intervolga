df6988124858908c317808ce63d9789876cecd22
Alexey Surin <543890@bk.ru>
2024-06-07 23:04:01 +0300

    add config file with params for connection to DB

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Сonfiguration file with constants to connect to DB
+ * 
+ */
+define('DB_HOST', 'localhost');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'comments');
